2c195395eb3bc25570edcb7379950c2e56ef8b07
rmholsen <jaguar.kitty@gmail.com>
2020-10-16 12:32:30 -0400

    Refactored confirm to separate method

--- lib/deejay/deejay.rb
+++ lib/deejay/deejay.rb
@@ -84,9 +84,6 @@ module Deejay
        puts "\n Here are some other artists in the #{genre} category:" 
        #Reformat the genre tag AFTER you put this dialogue
    
-       #sleep 3
-       #puts "\nPretend some artists are listed here."
-       #Simulating the scraper, take out this sleep and, obv, the text
        artists = Scraper.scrape_tag(genre)
        artists.each.with_index(1) do |artist, idx|
        puts "#{idx}. #{artist}"
@@ -97,161 +94,58 @@ module Deejay
    def genre
        puts "\nPlease select a genre (a description will be provided after you select): Electronic, Metal, Rock, Alternative, Hip-Hop, Experimental, Punk, Pop, Ambient"
        input = gets.strip.downcase
-   
+        #Okay, normally I wouldn't put in the descriptions since Rock, Metal, Blues, Jazz, Country, etc etc
+        #are pretty well known categories; Bandcamp has Rock, Metal, Hip-Hop, and a pile of weird categories
+        #so we're going to do definitions from them and add a step. Be fancy. 
+
        case input
            when "electronic"
                puts "Electronic music is music that employs electronic and digital musical instruments, circuitry-based musical technology. The generally accepted view of what qualifies as an electronic instrument, as opposed to an instrument that employs electronic assistance or aimplification, has changed over the years. Today's electronic music employs polyphonic synthesizers, digital mixers, and many tools ranging from commercial music software to experimental circuit-based technology. Electronic music includes the sub-categories of techno, dance, house, and bass'n'drum."
                puts "\nIs this the genre you wish to browse in? (Y/N):"
-               confirm = gets.strip.upcase
-               if confirm == "Y"
-                   puts "\nHere are some artists in the #{input} category."
-                   artists = Scraper.scrape_tag(input)
-                   artists.each.with_index(1) do |artist, idx|
-                   puts "#{idx}. #{artist}"
-                   end
-               elsif confirm == "N"
-                   genre
-               else 
-                   puts "I'm sorry, I didn't understand that."
-                   genre
-               end 
+               #A definition
+               confirm(input)
+               #Confirmation dialogue tree, plus calling the scraper 
                back_to_start
+               #And back to start with a "do you want to search again"
            when "metal"
                puts "\nMetal, or Heavy Metal, is a sub-genre of rock music that arose in the late 60s and early 70s as a development of gains in amplification and instrument technology. It is characterized by extended guitar solos, precise and intense percussion, and harsh vocals at high volume. (And, at a certain point in its progression, lots of hair.) Current metal sub-genres now include goth or doom metal, grindcore, and progressive metal."
                puts "\nIs this the genre you wish to browse in? (Y/N):"
-               confirm = gets.strip.upcase
-               if confirm == "Y"
-                   puts "\nHere are some artists in the #{input} category."
-                   artists = Scraper.scrape_tag(input)
-                   artists.each.with_index(1) do |artist, idx|
-                   puts "#{idx}. #{artist}"
-                   end
-               elsif confirm == "N"
-                   genre
-               else 
-                   puts "I'm sorry, I didn't understand that."
-                   genre
-               end 
+                confirm(input)
                back_to_start
            when "rock"
                puts "\nRock music grew out of blues, jazz, and country influences in the 1940s and 1950s, particularly in English-speaking countries, and is characterized by a verse-chorus based lyric style and usually a 4/4 beat. Rock music has since branched off into many, many sub-genres but remains a staple of the 20th century cultural history. Rock music tends to include little electronic influence beyond amplification and the occasional syntehsizer and typically involves electric guitar, bass guitar, drums, and vocals as its core sound. Rock sub-genres include pop, progressive rock, what has become known as classic rock (60s-early 90s), and punk rock."
                puts "\nIs this the genre you wish to browse in? (Y/N):"
-               confirm = gets.strip.upcase
-               if confirm == "Y"
-                   puts "\nHere are some artists in the #{input} category."
-                   artists = Scraper.scrape_tag(input)
-                   artists.each.with_index(1) do |artist, idx|
-                   puts "#{idx}. #{artist}"
-                   end
-                   
-               elsif confirm == "N"
-                   genre
-               else 
-                   puts "I'm sorry, I didn't understand that."
-                   genre
-               end 
+               confirm(input)
                back_to_start
            when "alternative"
                puts "\nAlternative is difficult to characterize as a genre, except in relation to other genres. Alternative grew out of the rock movement from the 70s to the 90s, providing a catch-all label for musicians and bands who branched out into other lyric forms or instrumentation. For these purposes alternative is a broad category for music adhering to no specific style criteria: goth, industrial, and indie rock are all examples of alternative music."
                puts "\nIs this the genre you wish to browse in? (Y/N):"
-               confirm = gets.strip.upcase
-               if confirm == "Y"
-                   puts "\nHere are some artists in the #{input} category."
-                   artists = Scraper.scrape_tag(input)
-                   artists.each.with_index(1) do |artist, idx|
-                   puts "#{idx}. #{artist}"
-                   end
-               elsif confirm == "N"
-                   genre
-               else 
-                   puts "I'm sorry, I didn't understand that."
-                   genre
-               end 
+               confirm(input)
                back_to_start
            when "hip-hop"
                puts "\nHip-Hop, or Rap music, was originally characterized by four elements: rapping or emceeing, DJing i.e. using record turntables and mixers to make music, the movement style known as breakdancing involving gymnastic and rhythmic dance, and graffiti signatures. It began as a form of cultural expression for urban communities and continues today in a distinctive lyric style involving rhythmic wordplay and building off of earlier works, including the practice of sampling." 
                puts "\nIs this the genre you wish to browse in? (Y/N):"
-               confirm = gets.strip.upcase
-               if confirm == "Y"
-                   puts "\nHere are some artists in the #{input} category."
-                   artists = Scraper.scrape_tag(input)
-                   artists.each.with_index(1) do |artist, idx|
-                   puts "#{idx}. #{artist}"
-                   end
-               elsif confirm == "N"
-                   genre
-               else 
-                   puts "I'm sorry, I didn't understand that."
-                   genre
-               end 
+               confirm(input)
                back_to_start
            when "experimental"
                puts "\nThe experimental category goes a step further than the alternative category, with no original musical style and encompassing a variety of arrangements of instrumentation, vocal stylings, and other forms of sound not traditionally regarded as music. Sub-categories include noise, drone, avant-garde, and sound art."
                puts "\nIs this the genre you wish to browse in? (Y/N):"
-               confirm = gets.strip.upcase
-               if confirm == "Y"
-                   puts "\nHere are some artists in the #{input} category."
-                   artists = Scraper.scrape_tag(input)
-                   artists.each.with_index(1) do |artist, idx|
-                   puts "#{idx}. #{artist}"
-                   end
-               elsif confirm == "N"
-                   genre
-               else 
-                   puts "I'm sorry, I didn't understand that."
-                   genre
-               end 
+               confirm(input) 
                back_to_start
            when "punk"
                puts "\nPunk as music form developed out of the overlapping fields of the punk movement and rock music. Punk contains the same core instrumentation and lyric style as rock music, but often with a political, anti-establishment element. As part of the anti-establishment ethos punk music tends to be produced through independent labels and independent studios rather than large corporations and conglomerates. This, by necessity, pushes punk artists to be innovative and resourceful, creating often unconventional sounds. Common punk sub-genres include emo, folk punk, ska, and garage."
                puts "\nIs this the genre you wish to browse in? (Y/N):"
-               confirm = gets.strip.upcase
-               if confirm == "Y"
-                   puts "\nHere are some artists in the #{input} category."
-                   artists = Scraper.scrape_tag(input)
-                   artists.each.with_index(1) do |artist, idx|
-                   puts "#{idx}. #{artist}"
-                   end
-               elsif confirm == "N"
-                   genre
-               else 
-                   puts "I'm sorry, I didn't understand that."
-                   genre
-               end 
+               confirm(input)
                back_to_start
            when "pop"
                puts "\nPop, although short for popular music, does not necessarily describe the popular music at any given time. It has come to describe a kind of music that employs a faster tempo than the standard rock 4/4, repeated chorus, and a vocal hook or distinctive riff. Pop music is designed to be highly commercial and marketable, and to capture a mood of the time. Therefore pop music also tends to be easily identifiable and commonly linked with a specific decade or year. Pop music includes the sub-genres synth pop, new wave, and country or culture-based sub-genres such as J(apanese)-Pop or K(orean)-Pop."
                puts "\nIs this the genre you wish to browse in? (Y/N):"
-               confirm = gets.strip.upcase
-               if confirm == "Y"
-                   puts "\nHere are some artists in the #{input} category."
-                   artists = Scraper.scrape_tag(input)
-                   artists.each.with_index(1) do |artist, idx|
-                   puts "#{idx}. #{artist}"
-                   end
-               elsif confirm == "N"
-                   genre
-               else 
-                   puts "I'm sorry, I didn't understand that."
-                   genre
-               end 
+               confirm(input)
                back_to_start
            when "ambient"
                puts "\nAmbient music prioritizes tone and sound over rhythm or structure, and tends to be instrumental in form. The intent of ambient music is often to express a mood or emotion, or to evoke a common moment or environment such as rain from the inside of a house or a coastal cliff. As with several other genres ambient music developed from the rapid advancements in computerization and electronics, enabling artists to create less instrument-based sounds. Ambient sub-genres are described in such terms as chill-out, atmospheric, dark-ambient, and idm."
                puts "\nIs this the genre you wish to browse in? (Y/N):"
-               confirm = gets.strip.upcase
-               if confirm == "Y"
-                   puts "\nHere are some artists in the #{input} category."
-                   artists = Scraper.scrape_tag(input)
-                   artists.each.with_index(1) do |artist, idx|
-                   puts "#{idx}. #{artist}"
-                   end
-               elsif confirm == "N"
-                   genre
-               else 
-                   puts "I'm sorry, I didn't understand that."
-                   genre
-               end 
+               confirm(input)
                back_to_start
            else
                puts "I'm sorry, I didn't understand that."
@@ -261,6 +155,7 @@ module Deejay
    end 
    
    def back_to_start
+       #The Back to Start method! Pretty self explanatory. 
        puts "\n Would you like to search again? (Y/N)"
        input = gets.strip.upcase
            if input == "Y"
@@ -273,6 +168,27 @@ module Deejay
            end 
    end 
        
+   def confirm(input)
+        #Confirmation dialogue tree
+        #Gets the input, formats it for the if statement
+        #Carries over the input variable from the main genre method
+        #Scrapes according to the genre tag and puts out a list of artists
+        #If this is not the genre the person wants to search in, starts over.
+        confirm = gets.strip.upcase
+        if confirm == "Y"
+            puts "\nHere are some artists in the #{input} category."
+            artists = Scraper.scrape_tag(input)
+            artists.each.with_index(1) do |artist, idx|
+            puts "#{idx}. #{artist}"
+            end
+        elsif confirm == "N"
+            genre
+        else 
+            puts "I'm sorry, I didn't understand that."
+            genre
+        end 
+    end 
+
   end 
 end 
 
